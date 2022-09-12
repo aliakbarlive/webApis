9d8e6797c09fdc35a10cbeb6a3a180a4b181a608
aliakbarlive <786akbara@gmail.com>
2022-09-12 16:05:14 -0400

    updated hashmap

--- test.js
+++ test.js
@@ -1,23 +1,18 @@
 // using hashmapping to find the unique key and its length
 
-let str = 'aaaabbssdfd'.split('').sort()
+let str = 'aaabassdfd'.split('').sort()
 let obj = {}
-
+let tested={}
 const hashMapping =(testStr) =>{
 
     testStr.map((item) =>{
         obj[item] = obj[item]? obj[item]+1: 1
-        // delete obj[item]
     })
-    // const res =  Object.entries(obj).map((item,i)=> item[1]).reduce((pre, next)=> pre>next?pre:next )
-    const res =  Object.keys(obj).reduce((key, value)=>  Math.max(key,obj[value]), -Infinity )
-    const tested = Object.keys(obj).filter((item) => obj[item]===res)
+    const value =  Object.entries(obj).map((item,i)=> item[1]).reduce((pre, next)=> pre>next?pre:next )
+    const max =  Object.keys(obj).reduce((key, value)=>  Math.max(key,obj[value]), -Infinity )
+    const objKey = Object.keys(obj).filter((item) => obj[item]===max)
+        tested[objKey]  = value
+    }
+    hashMapping(str)
     console.log(tested)
-}
 
-hashMapping(str)
-// find the larget or repeated value and only show the result
-
-// obj['a']= 5
-
-// console.log(obj)
